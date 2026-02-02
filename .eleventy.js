a208c11008d5e7a4eb046f3c2bb86cc408888213
Umar <71148652+umrua@users.noreply.github.com>
2026-02-02 01:41:45 +0000

    Alpine in HTML, name list + picker in .js

--- .eleventy.js
+++ .eleventy.js
@@ -3,5 +3,5 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
-  eleventyConfig.addPassthroughCopy("**/*.png");
+  eleventyConfig.addPassthroughCopy("utilities.js");
 };
